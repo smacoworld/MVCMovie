2fb9122bda5a934a30183f4511c5139d1505f5d4
rick-anderson <riande@microsoft.com>
2015-05-22 12:10:05 -0700

    auto migrations false

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -10,7 +10,7 @@ namespace MvcMovie.Migrations
     {
         public Configuration()
         {
-          AutomaticMigrationsEnabled = true;
+          AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(MvcMovie.Models.MovieDBContext context)
